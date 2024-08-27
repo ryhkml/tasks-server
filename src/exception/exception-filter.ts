2d200eea76a65e606ebfb1b775c3c49e5f88ce1e
ryhkml <reyhank95@hotmail.com>
2024-08-27 16:59:07 +0700

    Refactor: replaced inline error handling with `exceptionFilter` and simplified `fetch` function in `serve` configuration

---
+++ src/exception/exception-filter.ts
@@ -0,0 +1,57 @@
+import { Context } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { HTTPResponseError, TypedResponse } from "hono/types";
+import { StatusCode } from "hono/utils/http-status";
+import { InvalidJSONValue, JSONParsed, JSONValue, SimplifyDeepArray } from "hono/utils/types";
+
+type JSONRespondReturn<T extends JSONValue | SimplifyDeepArray<unknown> | InvalidJSONValue, U extends StatusCode> = Response & TypedResponse<SimplifyDeepArray<T> extends JSONValue ? JSONValue extends SimplifyDeepArray<T> ? never : JSONParsed<T> : never, U, 'json'>;
+type JSONRespondExceptionFilter = JSONRespondReturn<{ action: string, message: string | object }, StatusCode>;
+
+export function exceptionFilter(e: Error | HTTPResponseError, c: Context): JSONRespondExceptionFilter {
+	if (e instanceof HTTPException) {
+		if (e.status == 400) {
+			return c.json({
+				action: "Do not retry without fixing the problem",
+				message: e.cause || "A request includes an invalid credential or value"
+			}, 400);
+		}
+		if (e.status == 401) {
+			return c.json({
+				action: "The request should not be repeated",
+				message: "The request did not include valid authentication"
+			}, 401);
+		}
+		if (e.status == 403) {
+			return c.json({
+				action: "The request should not be repeated",
+				message: "The server did not accept valid authentication"
+			}, 403);
+		}
+		if (e.status == 409) {
+			return c.json({
+				action: "Do not retry without fixing the problem",
+				message: e.cause || "The request cannot be completed due to a conflict"
+			}, 409);
+		}
+		if (e.status == 413) {
+			return c.json({
+				action: "Do not retry without fixing the problem",
+				message: e.cause || "The request is too large"
+			}, 413);
+		}
+		if (e.status == 422) {
+			return c.json({
+				action: "Do not retry without fixing the problem",
+				message: e.cause || "The request did not meet one of it's preconditions"
+			}, 422);
+		}
+		return c.json({
+			action: "Do not retry this request more than once",
+			message: e.cause || "Internal server error"
+		}, 500);
+	}
+	return c.json({
+		action: "Do not retry this request more than once",
+		message: "Internal server error"
+	}, 500);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { env, serve } from "bun";
 
 import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
 import { prettyJSON } from "hono/pretty-json";
 import { requestId, RequestIdVariables } from "hono/request-id";
 import { secureHeaders } from "hono/secure-headers";
 import { BlankSchema } from "hono/types";
 
 import { owner } from "./apis/owner";
+import { exceptionFilter } from "./exception/exception-filter";
 import { toSafeInteger } from "./utils/common";
 
 const PORT = toSafeInteger(env.PORT) || 9220;
@@ -33,54 +33,7 @@ function main(): Hono<Var & { Variables: RequestIdVariables }, BlankSchema, "/">
 
 	api.notFound(() => new Response(null, { status: 404 }));
 
-	api.onError((e, c) => {
-		if (e instanceof HTTPException) {
-			if (e.status == 400) {
-				return c.json({
-					action: "Do not retry without fixing the problem",
-					message: e.cause || "A request includes an invalid credential or value"
-				}, 400);
-			}
-			if (e.status == 401) {
-				return c.json({
-					action: "The request should not be repeated",
-					message: "The request did not include valid authentication"
-				}, 401);
-			}
-			if (e.status == 403) {
-				return c.json({
-					action: "The request should not be repeated",
-					message: "The server did not accept valid authentication"
-				}, 403);
-			}
-			if (e.status == 409) {
-				return c.json({
-					action: "Do not retry without fixing the problem",
-					message: e.cause || "The request cannot be completed due to a conflict"
-				}, 409);
-			}
-			if (e.status == 413) {
-				return c.json({
-					action: "Do not retry without fixing the problem",
-					message: e.cause || "The request is too large"
-				}, 413);
-			}
-			if (e.status == 422) {
-				return c.json({
-					action: "Do not retry without fixing the problem",
-					message: e.cause || "The request did not meet one of it's preconditions"
-				}, 422);
-			}
-			return c.json({
-				action: "Do not retry this request more than once",
-				message: e.cause || "Internal server error"
-			}, 500);
-		}
-		return c.json({
-			action: "Do not retry this request more than once",
-			message: "Internal server error"
-		}, 500);
-	});
+	api.onError(exceptionFilter);
 
 	api.get("/status", c => c.text("OK"));
 
@@ -90,7 +43,7 @@ function main(): Hono<Var & { Variables: RequestIdVariables }, BlankSchema, "/">
 }
 
 serve({
-	async fetch(req, server): Promise<Response> {
+	fetch(req, server): Response | Promise<Response> {
 		return main().fetch(req, { ip: server.requestIP(req) });
 	},
 	port: PORT,
