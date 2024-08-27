36d8530bb94155767ba4f78c70fa0af10c727965
ryhkml <reyhank95@hotmail.com>
2024-08-27 17:05:25 +0700

    Refactor: removed unused `LOG` variable from `.env.example`, added `.env.test` to `.gitignore`, and updated the `install.sh` script to create `.env.test` with a different port and run database initialization using `.env.development`

--- install.sh
+++ install.sh
@@ -7,11 +7,12 @@ bun -v 1>/dev/null
 cat .env.example > .env.development
 echo ".env.development file has been created"
 
-cat .env.example > .env.production
-echo ".env.production file has been created"
+cat .env.example > .env.test
+sed -i "s/^PORT=[0-9]\+/PORT=9320/" .env.test
+echo ".env.test file has been created"
 
 bun install --frozen-lockfile
-bun run init:db:dev
+bun --env-file=.env.development run init-db.ts
 
 echo
 echo "Done"
