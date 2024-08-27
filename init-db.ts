7263adb9ac886aa1df2bc4b45cd7a825864836c9
ryhkml <reyhank95@hotmail.com>
2024-08-27 17:02:28 +0700

    Added force delete option for SQLite databases and improved handling for existing databases with warnings and clearer logs

--- init-db.ts
+++ init-db.ts
@@ -1,58 +1,57 @@
-import { env, file, sleep, spawnSync } from "bun";
-import { Database } from "bun:sqlite";
+import { argv, env, file, sleep, spawnSync } from "bun";
 
 import { existsSync } from "node:fs";
 import { rm } from "node:fs/promises";
-import { exit } from "node:process";
+
+import { timeframeDb } from "./src/utils/db";
 
 async function init(): Promise<void> {
 	try {
 		if (env.PATH_SQLITE == null) {
 			throw new Error("PATH_SQLITE to be defined");
 		}
-		const pathTasksDb = env.NODE_ENV == "development"
-			? env.PATH_SQLITE.replace(".db", "-test.db")
-			: env.PATH_SQLITE;
 		if (env.PRAGMA_KEY_SQLITE == null) {
 			throw new Error("PRAGMA_KEY_SQLITE to be defined");
 		}
-		// Timeframe
-		const pathTimeframeDb = pathTasksDb.replace(".db", "-timeframe.db");
-		if (env.NODE_ENV == "development") {
-			await rm(pathTimeframeDb, { force: true });
+		const forceDelete = argv[argv.length - 1] == "-f";
+		const pathTimeframeDb = env.PATH_SQLITE.replace(".db", "-timeframe.db");
+		if (forceDelete) {
+			await Promise.all([
+				rm(env.PATH_SQLITE, { force: true }),
+				rm(pathTimeframeDb, { force: true })
+			]);
 			await sleep(1);
 		}
-		if (!existsSync(pathTimeframeDb)) {
-			const db = new Database(pathTimeframeDb, { strict: true });
+		// Timeframe DB
+		if (existsSync(pathTimeframeDb)) {
+			console.warn("Timeframe DB is already exists");
+		} else {
+			const db = timeframeDb(pathTimeframeDb);
 			const raw = await file("src/sql/timeframe.sql").text();
 			db.run(raw);
 			db.run("INSERT INTO timeframe (id, lastRecordAt) VALUES (?1, ?2)", [1, Date.now()]);
 			db.close();
+			console.log("Timeframe DB ok");
 			await sleep(1);
 		}
-		// Tasks
-		if (env.NODE_ENV == "development") {
-			await rm(pathTasksDb, { force: true });
-			await sleep(1);
-		}
-		if (!existsSync(pathTasksDb)) {
+		// Tasks DB
+		if (existsSync(env.PATH_SQLITE)) {
+			console.warn("Tasks DB is already exists");
+		} else {
 			const options = ["-bail", "-nofollow", "-noheader", "-json"];
-			const { stderr, success } = spawnSync(["sqlcipher", pathTasksDb, ...options, `PRAGMA key = '${env.PRAGMA_KEY_SQLITE}'`, ".read src/sql/tasks.sql"], {
+			const { stderr, success } = spawnSync(["sqlcipher", env.PATH_SQLITE, ...options, `PRAGMA key = '${env.PRAGMA_KEY_SQLITE}'`, ".read src/sql/tasks.sql"], {
 				stdout: null,
 				env: {}
 			});
 			if (success) {
-				console.log("Done");
+				console.log("Tasks DB ok");
 				await sleep(1);
 			} else {
 				throw new Error(stderr.toString().trim());
 			}
-		} else {
-			console.warn("Already exists");
 		}
 	} catch (e) {
 		console.error(e);
-		exit(1);
 	}
 }
 
