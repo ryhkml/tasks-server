13f7d05c5d8c1bad2fe580b752cae2f2da1657b4
ryhkml <reyhank95@hotmail.com>
2024-08-27 12:20:49 +0700

    Refactor: introduced middleware to set `ownerId` in context and reorganized validation and token verification logic

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,28 +1,43 @@
 import { password } from "bun";
 
-import { MiddlewareHandler } from "hono";
+import { Context, MiddlewareHandler } from "hono";
 import { bearerAuth } from "hono/bearer-auth";
+import { every } from "hono/combine";
+import { createMiddleware } from "hono/factory";
 import { HTTPException } from "hono/http-exception";
 
+import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 
 import { query } from "./db";
+import { ownerId } from "../schemas/auth";
 
 export function tasksAuth(path?: string): MiddlewareHandler {
-	return bearerAuth({
-		async verifyToken(token, c) {
-			const schema = z.string().regex(/^[a-zA-Z0-9_-]{42}$/);
-			const result = schema.safeParse(token);
-			if (result.success) {
-				// @ts-expect-error
-				const ownerId = c.req.valid("header")["x-tasks-owner-id"] as string;
-				const owner = query<{ key: string }>(`SELECT key FROM owner WHERE id = '${ownerId}'`, path);
-				if (owner == null) {
-					throw new HTTPException(403);
-				}
-				return await password.verify(token, owner[0].key);
+	return every(
+		zValidator("header", ownerId, (result) => {
+			if (!result.success) {
+				throw new HTTPException(403);
 			}
-			throw new HTTPException(403);
-		}
-	});
+		}),
+		createMiddleware<Var>(async (c, next) => {
+			// @ts-expect-error
+			c.set("ownerId", c.req.valid("header")["x-tasks-owner-id"]);
+			await next();
+		}),
+		bearerAuth({
+			async verifyToken(token, c: Context<Var>) {
+				const schema = z.string().regex(/^[a-zA-Z0-9_-]{42}$/);
+				const result = schema.safeParse(token);
+				if (result.success) {
+					const id = c.get("ownerId");
+					const owner = query<{ key: string }>(`SELECT key FROM owner WHERE id = '${id}'`, path);
+					if (owner == null) {
+						throw new HTTPException(403);
+					}
+					return await password.verify(token, owner[0].key);
+				}
+				throw new HTTPException(403);
+			}
+		})
+	);
 }
