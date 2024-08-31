0a7c54c85722a59d3a14d18d76da2aa5674b3f2c
ryhkml <reyhank95@hotmail.com>
2024-08-31 16:50:37 +0700

    Update authorization schema to validate Bearer token format and key

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 
+const keySchema = z.string().regex(/^[a-zA-Z0-9_-]{42}$/);
+
 export const ownerId = z.object({
-	"authorization": z.string().refine(v => v.startsWith("Bearer ")),
+	"authorization": z.string().refine(v => {
+		const [prefix, key] = v.split(" ");
+		return prefix == "Bearer" && keySchema.safeParse(key).success;
+	}),
 	"x-tasks-owner-id": z.string().ulid()
 });
