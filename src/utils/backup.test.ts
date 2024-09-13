7ce3173c4827efa6a268e25578eee4b8df7a93fc
ryhkml <reyhank95@hotmail.com>
2024-09-14 01:58:59 +0700

    Changes to be committed: new file:   src/utils/backup.test.ts new file:   src/utils/backup.ts

---
+++ src/utils/backup.test.ts
@@ -0,0 +1,154 @@
+import { env, file, sleep, write } from "bun";
+import { Database } from "bun:sqlite";
+import { afterAll, beforeAll, describe, expect, it } from "bun:test";
+
+import { basename, dirname } from "node:path";
+import { rm } from "node:fs/promises";
+
+import { Storage } from "@google-cloud/storage";
+import { extract } from "tar";
+
+import { backupDb } from "./backup";
+
+describe("TEST BACKUP", () => {
+	describe.skipIf(env.BACKUP_METHOD_SQLITE == "GOOGLE_CLOUD_STORAGE")("", () => {
+		it("should successfully defined env variables for local backup", () => {
+			expect(env.PATH_SQLITE).toBeDefined();
+            expect(env.BACKUP_DIR_SQLITE).toBeDefined();
+            expect(env.BACKUP_METHOD_SQLITE).toBeDefined();
+            expect(env.BACKUP_METHOD_SQLITE).toMatch(/(LOCAL|GOOGLE_CLOUD_STORAGE)/i);
+		});
+	});
+
+	describe.skipIf(env.BACKUP_METHOD_SQLITE == "LOCAL")("", () => {
+		it("should successfully defined env variables for Google Cloud Storage backup", () => {
+			expect(env.BACKUP_GCS_PROJECT_ID_SQLITE).toBeDefined();
+			expect(env.BACKUP_GCS_PRIVATE_KEY_SQLITE).toBeDefined();
+			expect(env.BACKUP_GCS_CLIENT_ID_SQLITE).toBeDefined();
+			expect(env.BACKUP_GCS_CLIENT_EMAIL_SQLITE).toBeDefined();
+			expect(env.BACKUP_BUCKET_NAME_SQLITE).toBeDefined();
+			expect(env.BACKUP_BUCKET_DIR_SQLITE).toBeDefined();
+		});
+	});
+
+	describe.skipIf(env.BACKUP_METHOD_SQLITE == "GOOGLE_CLOUD_STORAGE")("", () => {
+		let pathBakDb = "";
+		beforeAll(async () => {
+			const path = await backupDb();
+			pathBakDb = path;
+		});
+		it("should successfully backup the database file to another directory", async () => {
+			const isExistsBakDb = await file(pathBakDb).exists();
+			expect(isExistsBakDb).toBe(true);
+			expect(pathBakDb).toEndWith(".db.tar.gz");
+		});
+		it("should successfully restore the database file", async () => {
+			await extract({
+				file: pathBakDb,
+				cwd: dirname(pathBakDb)
+			});
+			// Tasks
+			const pathDb1 = env.BACKUP_DIR_SQLITE + "/" + basename(env.PATH_SQLITE);
+			const db1 = new Database(pathDb1, { strict: true, create: false });
+			const raw1 = db1.prepare<{ name: string }, [string, string]>("SELECT name FROM sqlite_master WHERE type = ?1 AND name = ?2");
+			const owner = raw1.get("table", "owner");
+			expect(owner).not.toBeNull();
+			expect(owner?.name).toBe("owner");
+			const queue = raw1.get("table", "queue");
+			expect(queue).not.toBeNull();
+			expect(queue?.name).toBe("queue");
+			const config = raw1.get("table", "config");
+			expect(config).not.toBeNull();
+			expect(config?.name).toBe("config");
+			// Throttle
+			const pathDb2 = env.BACKUP_DIR_SQLITE + "/" + basename(env.PATH_SQLITE).replace(".db", "-throttle.db");
+			const db2 = new Database(pathDb2, { strict: true, create: false });
+			const raw2 = db2.query<{ name: string }, [string, string]>("SELECT name FROM sqlite_master WHERE type = ?1 AND name = ?2");
+			const control = raw2.get("table", "control");
+			expect(control).not.toBeNull();
+			expect(control?.name).toBe("control");
+			// Timeframe
+			const pathDb3 = env.BACKUP_DIR_SQLITE + "/" + basename(env.PATH_SQLITE).replace(".db", "-timeframe.db");
+			const db3 = new Database(pathDb3, { strict: true, create: false });
+			const raw3 = db3.query<{ name: string }, [string, string]>("SELECT name FROM sqlite_master WHERE type = ?1 AND name = ?2");
+			const timeframe = raw3.get("table", "timeframe");
+			expect(timeframe).not.toBeNull();
+			expect(timeframe?.name).toBe("timeframe");
+		});
+		afterAll(async () => {
+			await rm(dirname(pathBakDb), {
+				force: true,
+				recursive: true
+			});
+		});
+	});
+
+	describe.skipIf(env.BACKUP_METHOD_SQLITE == "LOCAL")("", () => {
+		let pathBakDb = "";
+		const storage = new Storage({
+			projectId: env.BACKUP_GCS_PROJECT_ID_SQLITE,
+			credentials: {
+				private_key: env.BACKUP_GCS_PRIVATE_KEY_SQLITE,
+				client_id: env.BACKUP_GCS_CLIENT_ID_SQLITE,
+				client_email: env.BACKUP_GCS_CLIENT_EMAIL_SQLITE,
+				type: "service_account"
+			},
+			timeout: 30000
+		});
+		const bucket = storage.bucket(env.BACKUP_BUCKET_NAME_SQLITE!);
+		beforeAll(async () => {
+			const path = await backupDb("GOOGLE_CLOUD_STORAGE");
+			pathBakDb = path;
+		});
+		it("should successfully backup the database file to Google Cloud Storage", async () => {
+			const [exists] = await bucket.file(env.BACKUP_BUCKET_DIR_SQLITE + "/" + basename(pathBakDb)).exists();
+			expect(exists).toBeTrue();
+		});
+		it("should successfully restore the database file from Google Cloud Storage", async () => {
+			const pathBucketBakDb = env.BACKUP_BUCKET_DIR_SQLITE + "/" + basename(pathBakDb);
+			const [buffer] = await bucket.file(pathBucketBakDb).download();
+			await write("/tmp/tasks/gcs/" + basename(pathBakDb), buffer);
+			await sleep(1);
+			await extract({
+				file: pathBakDb,
+				cwd: "/tmp/tasks/gcs"
+			});
+			// Tasks
+			const pathDb1 = "/tmp/tasks/gcs/" + basename(env.PATH_SQLITE);
+			const db1 = new Database(pathDb1, { strict: true, create: false });
+			const raw1 = db1.prepare<{ name: string }, [string, string]>("SELECT name FROM sqlite_master WHERE type = ?1 AND name = ?2");
+			const owner = raw1.get("table", "owner");
+			expect(owner).not.toBeNull();
+			expect(owner?.name).toBe("owner");
+			const queue = raw1.get("table", "queue");
+			expect(queue).not.toBeNull();
+			expect(queue?.name).toBe("queue");
+			const config = raw1.get("table", "config");
+			expect(config).not.toBeNull();
+			expect(config?.name).toBe("config");
+			// Throttle
+			const pathDb2 = "/tmp/tasks/gcs/" + basename(env.PATH_SQLITE).replace(".db", "-throttle.db");
+			const db2 = new Database(pathDb2, { strict: true, create: false });
+			const raw2 = db2.query<{ name: string }, [string, string]>("SELECT name FROM sqlite_master WHERE type = ?1 AND name = ?2");
+			const control = raw2.get("table", "control");
+			expect(control).not.toBeNull();
+			expect(control?.name).toBe("control");
+			// Timeframe
+			const pathDb3 = "/tmp/tasks/gcs/" + basename(env.PATH_SQLITE).replace(".db", "-timeframe.db");
+			const db3 = new Database(pathDb3, { strict: true, create: false });
+			const raw3 = db3.query<{ name: string }, [string, string]>("SELECT name FROM sqlite_master WHERE type = ?1 AND name = ?2");
+			const timeframe = raw3.get("table", "timeframe");
+			expect(timeframe).not.toBeNull();
+			expect(timeframe?.name).toBe("timeframe");
+		});
+		afterAll(async () => {
+			await Promise.all([
+				bucket.file(env.BACKUP_BUCKET_DIR_SQLITE + "/" + basename(pathBakDb)).delete(),
+				rm("/tmp/tasks/gcs", {
+					force: true,
+					recursive: true
+				})
+			]);
+		});
+	});
+});
---
+++ src/utils/backup.ts
@@ -0,0 +1,54 @@
+import { UTCDate } from "@date-fns/utc";
+import { env, write } from "bun";
+
+import { rmSync } from "node:fs";
+import { basename, dirname } from "node:path";
+
+import { Storage } from "@google-cloud/storage";
+import { create } from "tar";
+
+export async function backupDb(method?: string): Promise<string> {
+	const filename = basename(env.PATH_SQLITE).replace(".db", "-" + new UTCDate().toISOString()) + ".db.tar.gz";
+	if (method == "GOOGLE_CLOUD_STORAGE") {
+		const pathBakDb = "/tmp/tasks/gcs/" + filename;
+		await write("/tmp/tasks/gcs/.keep", "OK");
+		await create({
+			cwd: dirname(env.PATH_SQLITE),
+			file: pathBakDb,
+			follow: false,
+			gzip: true,
+			filter: path => !path.includes(".gitkeep")
+		}, ["./"]);
+		const storage = new Storage({
+			projectId: env.BACKUP_GCS_PROJECT_ID_SQLITE,
+			credentials: {
+				private_key: env.BACKUP_GCS_PRIVATE_KEY_SQLITE,
+				client_id: env.BACKUP_GCS_CLIENT_ID_SQLITE,
+				client_email: env.BACKUP_GCS_CLIENT_EMAIL_SQLITE,
+				type: "service_account"
+			},
+			timeout: 30000
+		});
+		await storage.bucket(env.BACKUP_BUCKET_NAME_SQLITE).upload(pathBakDb, {
+			destination: env.BACKUP_BUCKET_DIR_SQLITE + "/" + filename,
+			metadata: {
+				contentType: "application/tar+gzip"
+			}
+		});
+		rmSync(dirname(pathBakDb), {
+			force: true,
+			recursive: true
+		});
+		return pathBakDb;
+	}
+	const pathBakDb = env.BACKUP_DIR_SQLITE + "/" + filename;
+	await write(env.BACKUP_DIR_SQLITE + "/.keep", "OK");
+	await create({
+		cwd: dirname(env.PATH_SQLITE),
+		file: pathBakDb,
+		follow: false,
+		gzip: true,
+		filter: path => !path.includes(".gitkeep")
+	}, ["./"]);
+	return pathBakDb;
+}
