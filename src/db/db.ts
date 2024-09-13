92d94380dd3251cfbb939b4b7d8aa423febf32a5
ryhkml <reyhank95@hotmail.com>
2024-09-13 11:17:24 +0700

    Refactor logging and add option to database connections

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,19 +4,21 @@ import { Database } from "bun:sqlite";
 import { existsSync } from "node:fs";
 import { exit } from "node:process";
 
+import { logError } from "../utils/logger";
+
 if (!existsSync(env.PATH_SQLITE)) {
-	console.error("Tasks DB not found");
+	logError("Tasks DB not found");
 	exit(1);
 }
 if (!existsSync(env.PATH_SQLITE.replace(".db", "-throttle.db"))) {
-	console.error("Throttle DB not found");
+	logError("Throttle DB not found");
 	exit(1);
 }
 if (!existsSync(env.PATH_SQLITE.replace(".db", "-timeframe.db"))) {
-	console.error("Timeframe DB not found");
+	logError("Timeframe DB not found");
 	exit(1);
 }
 
-export const tasksDb = new Database(env.PATH_SQLITE, { strict: true });
-export const throttleDb = new Database(env.PATH_SQLITE.replace(".db", "-throttle.db"), { strict: true });
-export const timeframeDb = new Database(env.PATH_SQLITE.replace(".db", "-timeframe.db"), { strict: true });
+export const tasksDb = new Database(env.PATH_SQLITE, { strict: true, create: false });
+export const throttleDb = new Database(env.PATH_SQLITE.replace(".db", "-throttle.db"), { strict: true, create: false });
+export const timeframeDb = new Database(env.PATH_SQLITE.replace(".db", "-timeframe.db"), { strict: true, create: false });
