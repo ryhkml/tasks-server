4cbb2078c60e18d7fb3e431d24a57e7252b18809
ryhkml <reyhank95@hotmail.com>
2024-09-14 12:55:19 +0700

    Update tests to include throttleDb and refine queries for tasks and timeframe databases

--- src/db/db.test.ts
+++ src/db/db.test.ts
@@ -1,12 +1,11 @@
 import { env, file } from "bun";
 import { describe, expect, it } from "bun:test";
 
-import { tasksDb, timeframeDb } from "./db";
+import { tasksDb, throttleDb, timeframeDb } from "./db";
 
 describe("TEST DATABASE", () => {
 	describe("env variables", () => {
 		it("should successfully defined PATH_SQLITE", () => {
-			expect(env.PATH_SQLITE).not.toBeEmpty();
 			expect(env.PATH_SQLITE).toBeDefined();
 		});
 	});
@@ -14,25 +13,38 @@ describe("TEST DATABASE", () => {
 	describe("initialization", () => {
 		it("should successfully initialize tasks and timeframe database", async () => {
 			const db1 = await file(env.PATH_SQLITE).exists();
-			const db2 = await file(env.PATH_SQLITE.replace(".db", "-timeframe.db")).exists();
+			const db2 = await file(env.PATH_SQLITE.replace(".db", "-throttle.db")).exists();
+			const db3 = await file(env.PATH_SQLITE.replace(".db", "-timeframe.db")).exists();
 			expect(db1).toBeTrue();
 			expect(db2).toBeTrue();
+			expect(db3).toBeTrue();
 		});
 
 		it("should successfully query to the tasks database", () => {
-			const status = tasksDb.query<{ status: "Ok" }, []>("SELECT 'Ok' AS status").get();
-			expect(status).not.toBeNull();
-			expect(status).toStrictEqual({ status: "Ok" });
+			const stmt = tasksDb.prepare<{ name: string }, [string, string]>("SELECT name FROM sqlite_master WHERE type = ?1 AND name = ?2");
+			const owner = stmt.get("table", "owner");
+			expect(owner).not.toBeNull();
+			expect(owner?.name).toBe("owner");
+			const queue = stmt.get("table", "queue");
+			expect(queue).not.toBeNull();
+			expect(queue?.name).toBe("queue");
+			const config = stmt.get("table", "config");
+			expect(config).not.toBeNull();
+			expect(config?.name).toBe("config");
+		});
+
+		it("should successfully query to the throttle database", async () => {
+			const raw = throttleDb.query<{ name: string }, [string, string]>("SELECT name FROM sqlite_master WHERE type = ?1 AND name = ?2");
+			const control = raw.get("table", "control");
+			expect(control).not.toBeNull();
+			expect(control?.name).toBe("control");
 		});
 
 		it("should successfully query to the timeframe database", async () => {
-			const timeframe = timeframeDb.query<{ id: 1, lastRecordAt: number }, []>("SELECT * FROM timeframe").get();
+			const raw = timeframeDb.query<{ name: string }, [string, string]>("SELECT name FROM sqlite_master WHERE type = ?1 AND name = ?2");
+			const timeframe = raw.get("table", "timeframe");
 			expect(timeframe).not.toBeNull();
-			expect(timeframe).toBeDefined();
-			expect(timeframe).toHaveProperty("id");
-			expect(timeframe?.id).toBe(1);
-			expect(timeframe).toHaveProperty("lastRecordAt");
-			expect(timeframe?.lastRecordAt).toBeTypeOf("number");
+			expect(timeframe?.name).toBe("timeframe");
 		});
 	});
 });
