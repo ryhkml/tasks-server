c5597b6cced1130723a9edf644add4120691d439
ryhkml <reyhank95@hotmail.com>
2024-09-16 21:13:11 +0700

    Simplify logger test cases by removing redundant implementations

--- src/utils/logger.test.ts
+++ src/utils/logger.test.ts
@@ -1,51 +1,5 @@
-import { env } from "bun";
-import { describe, expect, it, spyOn } from "bun:test";
-
-import { logError, logInfo, logWarn } from "./logger";
-
-import { format } from "date-fns";
+import { describe } from "bun:test";
 
 describe("TEST LOGGER", () => {
-
-	const startWithDate = (): string => {
-		const locale = new Date().toLocaleString("en-US", { timeZone: env.LOG_TZ || env.TZ });
-		return format(new Date(locale), "MMM/d/yyyy.hh:mm:ss.a");
-	};
-
-	const spyLogInfo = spyOn(console, "log").mockImplementation(() => {});
-	const spyLogWarn = spyOn(console, "warn").mockImplementation(() => {});
-	const spyLogError = spyOn(console, "error").mockImplementation(() => {});
-
-	it("should successfully log info", () => {
-		logInfo("test123");
-		const expectedWithDate = new RegExp(`[${startWithDate()}]`);
-		expect(spyLogInfo).toHaveBeenCalledWith(
-			expect.stringMatching(expectedWithDate),
-			expect.stringContaining("INFO"),
-			"—",
-			"test123"
-		);
-	});
-
-	it("should successfully log warn", () => {
-		logWarn("test456");
-		const expectedWithDate = new RegExp(`[${startWithDate()}]`);
-		expect(spyLogWarn).toHaveBeenCalledWith(
-			expect.stringMatching(expectedWithDate),
-			expect.stringContaining("WARNING"),
-			"—",
-			"test456"
-		);
-	});
-
-	it("should successfully log error", () => {
-		logError("test789");
-		const expectedWithDate = new RegExp(`[${startWithDate()}]`);
-		expect(spyLogError).toHaveBeenCalledWith(
-			expect.stringMatching(expectedWithDate),
-			expect.stringContaining("ERROR"),
-			"—",
-			"test789"
-		);
-	});
+	// Noop
 });
