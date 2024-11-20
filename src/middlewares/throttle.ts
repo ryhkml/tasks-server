fff03c64d53b5fb252bc24debff47d6ca0f1e4b2
ryhkml <reyhank95@hotmail.com>
2024-11-20 12:48:50 +0700

    Improve throttle logic to handle zero limits gracefully

--- src/middlewares/throttle.ts
+++ src/middlewares/throttle.ts
@@ -15,8 +15,13 @@ export const db = new Database(env.PATH_SQLITE.replace(".db", "-throttle.db"), {
 });
 
 export async function throttle(c: Context<Var>, next: Next): Promise<void> {
-	const MAX_REQUEST = safeInteger(env.MAX_THROTTLE_REQUEST) || 10;
-	const TIME_WINDOW = safeInteger(env.MAX_THROTTLE_TIME_WINDOW) || 60000;
+	const MAX_REQUEST = safeInteger(env.MAX_THROTTLE_REQUEST);
+	const TIME_WINDOW = safeInteger(env.MAX_THROTTLE_TIME_WINDOW);
+
+	if (MAX_REQUEST == 0 && TIME_WINDOW == 0) {
+		await next();
+		return;
+	}
 
 	const id = c.get("clientId");
 	const todayAt = c.get("todayAt");
@@ -33,15 +38,18 @@ export async function throttle(c: Context<Var>, next: Next): Promise<void> {
 	c.header("RateLimit-Limit", MAX_REQUEST.toString());
 
 	if (control) {
-		if ((todayAt - control.lastRequestAt) < TIME_WINDOW) {
+		if (todayAt - control.lastRequestAt < TIME_WINDOW) {
 			if (control.requestCount >= MAX_REQUEST) {
 				const retryAfter = Math.ceil((TIME_WINDOW - (todayAt - control.lastRequestAt)) / 1000);
 				c.header("RateLimit-Remaining", "0");
 				c.header("RateLimit-Reset", retryAfter.toString());
-				logWarn("(429) Request temporarily blocked", JSON.stringify({
-					ip: c.get("ip"),
-					userAgent: c.get("userAgent")
-				}));
+				logWarn(
+					"(429) Request temporarily blocked",
+					JSON.stringify({
+						ip: c.get("ip"),
+						userAgent: c.get("userAgent")
+					})
+				);
 				throw new HTTPException(429);
 			}
 			const stmtRequestCount = db.query<Pick<ControlTable, "requestCount">, string>(`
@@ -53,18 +61,11 @@ export async function throttle(c: Context<Var>, next: Next): Promise<void> {
 			const { requestCount } = stmtRequestCount.get(id)!;
 			c.header("RateLimit-Remaining", (MAX_REQUEST - requestCount).toString());
 		} else {
-			db.run("UPDATE control SET requestCount = 1, lastRequestAt = ?1 WHERE id = ?2", [
-				todayAt,
-				id
-			]);
+			db.run("UPDATE control SET requestCount = 1, lastRequestAt = ?1 WHERE id = ?2", [todayAt, id]);
 			c.header("RateLimit-Remaining", (MAX_REQUEST - 1).toString());
 		}
 	} else {
-		db.run("INSERT INTO control (id, requestCount, lastRequestAt) VALUES (?1, ?2, ?3)", [
-			id,
-			1,
-			todayAt
-		]);
+		db.run("INSERT INTO control (id, requestCount, lastRequestAt) VALUES (?1, ?2, ?3)", [id, 1, todayAt]);
 		c.header("RateLimit-Remaining", (MAX_REQUEST - 1).toString());
 	}
 
